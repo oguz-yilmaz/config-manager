d422a1d460ba734446a83a34fdd0e10cb1769b25
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-03-10 10:45:23 +0300

    Add basic config retrieval

--- src/config_system/storage.py
+++ src/config_system/storage.py
@@ -4,3 +4,6 @@ from typing import Optional
 class Storage:
     def __init__(self, host: str = "localhost"):
         self.client = etcd3.client(host=host)
+    def get_config(self, path: str) -> Optional[dict]:
+        value = self.client.get(f"/config/{path}")
+        return value[0] if value[0] else None
