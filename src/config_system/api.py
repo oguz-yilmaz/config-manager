528230f98ad0c05559a8e579c73cf1ff591ea70b
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-07-18 16:45:23 +0300

    Add get config endpoint

--- src/config_system/api.py
+++ src/config_system/api.py
@@ -2,3 +2,6 @@ from fastapi import FastAPI
 from .config_manager import ConfigManager
 
 app = FastAPI()
+@app.get("/config/{path:path}")
+async def get_config(path: str):
+    return {"path": path}
