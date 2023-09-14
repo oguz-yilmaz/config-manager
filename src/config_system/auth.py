9790beb95b53ad50d66a6345d878a262d0bfa5ff
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-09-14 16:34:21 +0300

    Add key verification

--- src/config_system/auth.py
+++ src/config_system/auth.py
@@ -3,3 +3,5 @@ from typing import List
 class AuthManager:
     def __init__(self):
         self.api_keys = {}
+    def verify_key(self, api_key: str) -> bool:
+        return api_key in self.api_keys
