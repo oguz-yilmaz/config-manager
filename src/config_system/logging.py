c2932abbe39c3664ed9cfda671860798d702acfc
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-12-16 16:12:33 +0300

    Add logging configuration

---
+++ src/config_system/logging.py
@@ -0,0 +1,8 @@
+import logging
+logger = logging.getLogger(__name__)
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
