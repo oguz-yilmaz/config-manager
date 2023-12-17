2b9c8e2c497f5f1f4cbaa483b14263648c1f40a1
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-12-17 16:34:56 +0300

    Implement API key authentication

--- src/config_system/auth.py
+++ src/config_system/auth.py
@@ -5,3 +5,12 @@ class AuthManager:
         self.api_keys = {}
     def verify_key(self, api_key: str) -> bool:
         return api_key in self.api_keys
+from fastapi.security import APIKeyHeader
+from typing import Optional
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+async def verify_api_key(api_key: str = Depends(api_key_header)) -> str:
+    if not is_valid_key(api_key):
+        raise HTTPException(status_code=403)
+    return api_key
