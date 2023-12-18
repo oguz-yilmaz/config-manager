c2c3bc9dc26c5e4d5dbc2a41ac73cbcabdb137fa
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-12-18 10:23:11 +0300

    Add version model

--- src/config_system/models.py
+++ src/config_system/models.py
@@ -9,3 +9,11 @@ class AuditLog(BaseModel):
     action: str
     timestamp: datetime
     user: str
+class Version:
+    def __init__(self, major: int, minor: int, patch: int):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+    
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
