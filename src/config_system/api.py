e61f73c25f7c3bf17ada74f5d0ea9d77d5692e95
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-12-19 16:34:56 +0300

    Add rate limiting

--- src/config_system/api.py
+++ src/config_system/api.py
@@ -15,3 +15,14 @@ async def health_check():
             status_code=503,
             content={"status": "unhealthy"}
         )
+from fastapi import Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+
+@app.get("/config/{path:path}")
+@limiter.limit("100/minute")
+async def get_config(request: Request, path: str):
+    pass
